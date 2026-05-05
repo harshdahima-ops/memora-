47628f238a0b3951f45bb643f235c9a1b1454ce7
harshdahima-ops <harshdahima509@gmail.com>
2026-05-05 11:18:35 +0530

    Change Supabase key to a JWT
    Updated Supabase key from a publishable key to a JWT.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://lzqeiaoqpzltrwtvlvmx.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_3bPpzCCwdd16RXtVq_m-ng_4CaJrowN'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cWVpYW9xcHpsdHJ3dHZsdm14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NDgwNzEsImV4cCI6MjA5MjQyNDA3MX0.kysFIp9kxOuYy6D09RS_4RgsKvQnwKaG0DewaTT6tok'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
